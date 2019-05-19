076f9e41cfc061aa92db6d05668827198f4ff63a
aaronseg <aaron.seggelke@smail.th-koeln.de>
2019-05-19 15:23:55 +0200

    add data folder for testing

---
+++ Server/data/index.js
@@ -0,0 +1,14 @@
+/**
+ * Created by fmark on 29.06.2017.
+ */
+const users = new Array();
+const playlist = new Array();
+var loggedUser;
+var count;
+
+exports.users = [];
+exports.playlist = [];
+exports.count = 0;
+exports.loggedUser = -1;
+
+
